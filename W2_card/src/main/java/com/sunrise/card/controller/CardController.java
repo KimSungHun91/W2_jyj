1c2914cb04057f125fc9e533c8c915e6d7add6fb
정여진 <admin@DESKTOP-H31F5PL>
2022-10-27 21:00:25 +0900

    수정 커밋_주석제거

--- W2_card/src/main/java/com/sunrise/card/controller/CardController.java
+++ W2_card/src/main/java/com/sunrise/card/controller/CardController.java
@@ -25,25 +25,11 @@ public class CardController {
 	}
 	@RequestMapping(value = "/send", method = RequestMethod.POST)
 	public String form1(RcvapplVo rcvapplVo) {
-		/*
-		 * 제가 select 까지 넣어놨어요~!!
-		 * 한번 로직 쭉 보시구 아래 주석들 참고하셔서
-		 * 불능 체크 후에 insert 한번 해보시면 될거같아요 :)
-		 * 화이팅~! 여진띠 
-		 */
-		
-		// 접수일자 별 해당 고객의 입회신청서 내역 select
-		List<RcvapplVo> testDate = cService.test(rcvapplVo);
-		
-		// select 해온 입회신청서 내역의 길이를 체크(불능인 입회신청서를 하드코딩 해둠) ex) testDate.size()
-		// if문을 작성해보세요~!
-		// 정상 입력된 건이 하나도 없으면 해당 고객의 입회신청서 insert
-		if(false) {
-			cService.deal(rcvapplVo);
-		}
+		cService.deal(rcvapplVo);		
+		cService.deal(rcvapplVo);	// 고객
 		
 		return "inquire";
-	}
+
 	/*
 	@RequestMapping(value = "/a", method = RequestMethod.POST)
 	public String form1(@ModelAttribute CardVO cardVo) {
@@ -61,6 +47,6 @@ public class CardController {
 		return "inquire";
 	}
 	*/
-	
+	}
 	
 }
